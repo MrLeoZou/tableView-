9b400129e057ff25e3f47d90e6b3b824ac7f0aed
MrLeoZou <apple@appledeMacBook-Pro.local>
2015-03-23 01:00:03 +0800

    Initial Commit

---
+++ tableView数据源封装/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tableView数据源封装
+//
+//  Created by apple on 15/3/23.
+//  Copyright (c) 2015年 zzp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tableView数据源封装/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  tableView数据源封装
+//
+//  Created by apple on 15/3/23.
+//  Copyright (c) 2015年 zzp. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
